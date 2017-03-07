58d38b159d57be2db74b58ec55885a7294490919
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-03-07 20:34:18 +0100

    Update ABKcontrol:
    - Define pin constants
    - Add ABK typedefs and enums
    - Add helpers functions

--- src/ABKcontrol.cpp
+++ src/ABKcontrol.cpp
@@ -26,6 +26,23 @@ void ABK_set_drum_mode(ABK_drum_mode_t mode) {
             brake = 1;
             break;
     }
-
 }
 
+int ABK_set_speed(float speed) {
+    float dt = 0.0;
+
+    dt = ABK_map(0, 100, ABK_MOT_MIN_DT, ABK_MOT_MAX_DT, speed);
+    dt = dt / 100;
+
+    if (dt > 1.0)
+        dt = 1.0;
+    else if (dt < 0.0)
+        dt = 0;
+
+    motor_ctl = dt;
+    return 0;
+}
+
+float ABK_map(int from_min, int from_max, int to_min, int to_max, int value) {
+    return to_min + (float) ((value-from_min)/(from_max-from_min)) * (float) (to_max-to_min);
+}
--- src/ABKcontrol.h
+++ src/ABKcontrol.h
@@ -8,7 +8,36 @@
 #ifndef ABKCONTROL_H
 #define ABKCONTROL_H
 
-#include "pins.h"
+#include "mbed.h"
+
+#define ABK_MOT_MIN_DT      (50)
+#define ABK_MOT_MAX_DT      (80)
+
+extern DigitalOut brake;
+extern DigitalOut clutch;
+extern DigitalOut dir_fw;
+extern DigitalOut dir_rw;
+extern PwmOut motor_ctl;
+
+typedef struct {
+    uint16_t time;
+    uint8_t speed;
+} ABK_point_t;
+
+typedef struct {
+    uint16_t start_time;
+    ABK_point_t p1;
+    ABK_point_t p2;
+    uint16_t stop_time;
+} ABK_config_t;
+
+typedef enum {
+    ABK_STATE_STANDBY = 0,
+    ABK_STATE_NOT_CONFIGURED,
+    ABK_STATE_CONFIGURED,
+    ABK_STATE_RUN,
+    ABK_STATE_RESET,
+} ABK_state_t;
 
 typedef enum {
     ABK_DRUM_BRAKED,
@@ -18,5 +47,8 @@ typedef enum {
 } ABK_drum_mode_t;
 
 void ABK_set_drum_mode(ABK_drum_mode_t);
+int ABK_set_speed(float speed);
+
+float ABK_map(int from_min, int from_max, int to_min, int to_max, int value);
 
 #endif /* !ABKCONTROL_H */
