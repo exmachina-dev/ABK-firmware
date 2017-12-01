daddf0f63c7498113cbf87de4b264c7652f24b4a
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-12-01 14:15:15 +0100

    Use nxp-flasher as a submodule

--- make.bat
+++ make.bat
@@ -5,7 +5,7 @@ mbed compile
 @echo Done.
 
 @echo Flashing to %1...
-python.exe tools\nxpprog.py --baud=230400 --cpu=lpc1768 --oscfreq=120000 %1 BUILD\LPC1768\GCC_ARM\ABK-firmware.bin
+python.exe tools\nxp-flasher\nxpprog.py --baudrate 230400 --cpu lpc1768 --oscfreq 120000 %1 BUILD\LPC1768\GCC_ARM\ABK-firmware.bin
 @echo Done.
 
 
